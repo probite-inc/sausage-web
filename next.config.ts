500eb46269ce100c56c2f9ef60c8c939fa488f7c
ArenRover <74602554+AreNRover@users.noreply.github.com>
2025-09-10 11:51:38 -0700

    set up config for githubpages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
+export const config = {
+  isProd: true,
+  basePath: "/susage-web",
+};
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: "export",
+  basePath: config.isProd ? config.basePath : "",
 };
 
 export default nextConfig;
