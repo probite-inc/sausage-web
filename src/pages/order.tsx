5b016396bce08d44d268d0bf9101c02fed6ad9fa
maki596 <michael.scofieldms568@gmail.com>
2025-10-16 00:50:28 +0330

    add a form

--- src/pages/order.tsx
+++ src/pages/order.tsx
@@ -1,16 +1,9 @@
+import { FilloutStandardEmbed } from "@fillout/react";
+
 export default function Order() {
   return (
     <div className="w-full h-screen flex items-center justify-center text-2xl">
-      <iframe
-        src="https://docs.google.com/forms/d/e/1FAIpQLSfGAQhUbIDMxmH-jflqwg5iZmzEgqp85kCgeL0CIoZ2uGR_5A/viewform?embedded=true"
-        width="700"
-        height="520"
-        frameborder="0"
-        marginheight="0"
-        marginwidth="0"
-      >
-        Loading…
-      </iframe>
+      <FilloutStandardEmbed filloutId="4uL7JTXkwGus" />
     </div>
   );
 }
